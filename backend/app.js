8713f6cb6c488f5acce3f7d9eed115eb171f2c32
AndreyB <AnB.124@yandex.ru>
2022-04-07 22:36:38 +0700

    рефакторинг

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
