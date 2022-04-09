1eaa67434c2986314b1602f83ccd634403456ade
AndreyB <AnB.124@yandex.ru>
2022-04-09 18:09:39 +0700

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,21 +1,48 @@
+// const jwt = require('jsonwebtoken');
+
+// const { NODE_ENV, JWT_SECRET } = process.env;
+// const UnauthorizedError = require('../errors/UnauthorizedError');
+
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   if (!authorization && !authorization.startsWith('Bearer ')) {
+//     return new UnauthorizedError('Требуется авторизация');
+//   }
+//   const token = authorization.replace('Bearer ', '');
+//   let payload;
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
+//   } catch (err) {
+//     return new UnauthorizedError('Требуется авторизация');
+//   }
+//   req.user = payload;
+//   next();
+// };
+
 const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-const UnauthorizedError = require('../errors/UnauthorizedError');
 
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization && !authorization.startsWith('Bearer ')) {
-    return new UnauthorizedError('Требуется авторизация');
+const { UnauthorizedError } = require('../errors/UnauthorizedError');
+
+function auth(req, res, next) {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new UnauthorizedError('Ошибка авторизации: не найден req.cookies.jwt');
   }
-  const token = authorization.replace('Bearer ', '');
+
   let payload;
+
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return new UnauthorizedError('Требуется авторизация');
+    throw new UnauthorizedError('Ошибка верификации токена');
   }
+
   req.user = payload;
   next();
-};
+}
+
+module.exports = { auth };
