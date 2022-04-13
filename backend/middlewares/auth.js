719257cdb73d86aebeb93a07fbdf4166044dd520
AndreyB <AnB.124@yandex.ru>
2022-04-13 23:35:41 +0700

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,49 +1,51 @@
-const jwt = require('jsonwebtoken');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
-const UnauthorizedError = require('../errors/UnauthorizedError');
-
-function auth(req, res, next) {
-  const { authorization } = req.headers;
-  if (!authorization && !authorization.startsWith('Bearer ')) {
-    return new UnauthorizedError('Требуется авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-  try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
-  } catch (err) {
-    return new UnauthorizedError('Требуется авторизация');
-  }
-  req.user = payload;
-  return next();
-}
-
-module.exports = { auth };
-
 // const jwt = require('jsonwebtoken');
 
 // const { NODE_ENV, JWT_SECRET } = process.env;
-
 // const UnauthorizedError = require('../errors/UnauthorizedError');
 
 // function auth(req, res, next) {
-//   const token = req.cookies.jwt;
-
-//   if (!token) {
-//     throw new UnauthorizedError('Ошибка авторизации: не найден req.cookies.jwt');
+//   const { authorization } = req.headers;
+//   if (!authorization && !authorization.startsWith('Bearer ')) {
+//     return new UnauthorizedError('Требуется авторизация');
 //   }
-
+//   const token = authorization.replace('Bearer ', '');
 //   let payload;
-
 //   try {
 //     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
 //   } catch (err) {
-//     throw new UnauthorizedError('Требуется авторизация');
+//     return new UnauthorizedError('Требуется авторизация');
 //   }
-
 //   req.user = payload;
 //   next();
 // }
 
 // module.exports = { auth };
+
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+function auth(req, res, next) {
+  // const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  const token = authorization.replace('Bearer ', '');
+
+  if (!token) {
+    throw new UnauthorizedError('Ошибка авторизации: не найден token');
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'my-secret');
+  } catch (err) {
+    throw new UnauthorizedError('Требуется авторизация');
+  }
+
+  req.user = payload;
+  next();
+}
+
+module.exports = { auth };
