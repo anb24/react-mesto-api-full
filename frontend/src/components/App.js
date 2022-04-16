a2544e9c5d39a46e0201255a5b26132e254846f1
AndreyB <AnB.124@yandex.ru>
2022-04-16 13:53:30 +0700

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -227,7 +227,7 @@ function App() {
     }
 
     function handleCardLike(card) {
-        const isLiked = card.likes.some((i) => i === currentUser._id);
+        const isLiked = card.likes.some((i) => i._id === currentUser._id);
         api
             .changeCardLike(card._id, !isLiked)
             .then((newCardSomeLike) => {
